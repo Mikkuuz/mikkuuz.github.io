1b5c0460832cfd9237d9c013a7d474879ad7ff1d
hurtti <hurjacode@gmail.com>
2022-10-15 22:07:23 +0300

    update ja kaikkee

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const redirect = async (name) => {
     await delay(1600);
     location.href=name;
 }
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    h + "." + m + "." + s;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
